c1c4f42c60f660cf727c6403b8bd711b2f5c4a45
BernasconiLorenzo06 <bernasconi.lorenzo@iisgalvanimi.edu.it>
2023-02-10 12:36:46 +0000

    primo commit

--- src/app/mock-cars.ts
+++ src/app/mock-cars.ts
@@ -1,9 +1,9 @@
 import { Car } from './car';
 
 export const CARS: Car[] = [
-  { id: 11, name: 'Putno', costo:10, consumo:25 },
-  { id: 12, name: 'Bravo', costo:20, consumo:20 }, 
-  { id: 13, name: 'Tipo', costo:15, consumo:10 }, 
-  { id: 14, name: '124_spider' , costo:100, consumo:90 },
-  { id: 15, name: '500', costo:1000, consumo:33 }
+  { id: 11, name: 'Pino', costo:10, consumo:25 },
+  { id: 12, name: 'Gino', costo:20, consumo:20 }, 
+  { id: 13, name: 'Fino', costo:15, consumo:10 }, 
+  { id: 14, name: 'Zino' , costo:100, consumo:90 },
+  { id: 15, name: 'Lino', costo:1000, consumo:33 }
 ];
